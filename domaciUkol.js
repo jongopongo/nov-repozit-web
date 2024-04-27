d9ec4f55a2004daed86908f04b80ce4d9e5ddc31
Klára <pepsinka@gmail.com>
2024-04-27 21:39:22 +0200

    update DU

--- domaciUkol.js
+++ domaciUkol.js
@@ -2,12 +2,15 @@
 
 // Zobrazení promptu pro zadání teploty ve stupních Fahrenheita
 let fahrenheit = prompt("Zadejte teplotu ve stupních Fahrenheita:");
+fahrenheit = parseFloat(fahrenheit); //převod na číslo
+
+//převede číslo 
 let celsius = (fahrenheit - 32) * 5 / 9;
 
 // Zaokrouhlení na dva desetinná místa
 celsius = Math.round(celsius * 100) / 100;
 
 
-
+console.log(fahrenheit + 23);
 
 document.body.innerHTML += "<p>Převod Fahrenheit na Celsius: " + fahrenheit + " stupňů Fahrenheit = " + celsius + " stupňů Celsia</p>";
