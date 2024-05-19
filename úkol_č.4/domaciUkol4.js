7608765cebba8eda8e8644faf55b2ff94546aa26
Klára <pepsinka@gmail.com>
2024-05-19 19:36:01 +0200

    update ukol c.4

--- úkol_č.4/domaciUkol4.js
+++ úkol_č.4/domaciUkol4.js
@@ -1,4 +1,4 @@
-// Zadání čísel
+/* Zadání čísel
 let a = 55;
 let b = 15;
 let c = 5;
@@ -18,3 +18,27 @@ if (max < c) {
 
 // Výpis největšího čísla
 console.log("Největší číslo je:", max);
+*/
+
+
+// another :-)
+
+function max3(a, b, c) {
+    let max;
+    if (a > b) {
+        max = a;
+    } else {
+        max = b;
+    }
+    
+    if (max < c) {
+        max = c;
+    }
+    
+    return max;
+}
+
+// Use case
+let result = max3(100, 45, 18);
+console.log(result);
+
