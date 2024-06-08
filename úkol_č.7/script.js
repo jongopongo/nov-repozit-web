1f635b736962aac0740eb0f2d64d8ac0f3611d6a
Klára <pepsinka@gmail.com>
2024-06-08 16:34:57 +0200

    update ukol c.7 newsletter

--- úkol_č.7/script.js
+++ úkol_č.7/script.js
@@ -1,5 +1,5 @@
 
-    /*kod pridava forlumar s textovym polickem a tlacitkem k prihlaseni a odberu*/
+    //kod prida formular s textovym polem a tlacitkem k prihlaseni a odberu
 	document.getElementById('newsletter-form').addEventListener('submit', function (event) {
         event.preventDefault(); // Zabrání odeslání formuláře
     
@@ -9,21 +9,22 @@
         console.log(`Uživatel s e-mailem ${email} byl úspěšně přihlášen k odběru.`);
     });
     
-    /*Zpráva o úspěšném přihlášení*/
+    //Zpráva o úspěšném přihlášení
     
     document.getElementById('newsletter-form').addEventListener('submit', function (event) {
-        event.preventDefault(); /*zabrání odeslání emailu */
+        event.preventDefault(); //melo by zabranit odeslani emailu
         
         const email = document.getElementById('email').value;
         const successMessage = document.createElement('p');
         successMessage.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`;
-        document.body.appendChild(successMessage);
-    });
-
-    const emailInput = document.querySelector('#email'); // Vyberte textové pole podle jeho ID (v tomto případě 'email')
+       
+     document.body.appendChild(successMessage); //zpráva o přihlášení bude viditelná na stránce
+     });
+     
+    const emailInput = document.querySelector('#email');
 
     emailInput.addEventListener('input', (event) => {
-    // Tady můžete provést akce, které se mají stát při změně hodnoty v poli
+    
     const enteredValue = event.target.value;
     console.log(`Uživatel zadal: ${enteredValue}`);
 });
